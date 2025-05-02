62773d98a757a75ce0d8b390b44fd10ba872e4b2
Prince Kumar <princesd991@gmail.com>
2025-05-02 15:04:13 +0530

    add separate josn data

---
+++ server/data/BalanceData.ts
@@ -0,0 +1,19 @@
+export const balanceDataFirst = [
+  { value: 15 },
+  { value: 40 },
+  { value: 25 },
+  { value: 30 },
+  { value: 55 },
+  { value: 60 },
+  { value: 30 },
+];
+
+export const balanceDataSecond = [
+  { value: 0 },
+  { value: 65 },
+  { value: 10 },
+  { value: 40 },
+  { value: 20 },
+  { value: 40 },
+  { value: 50 },
+];
---
+++ server/data/HeartRateData.ts
@@ -0,0 +1,9 @@
+export const HeartRateData = [
+  { value: 20 },
+  { value: 40 },
+  { value: 75 },
+  { value: 97 },
+  { value: 55 },
+  { value: 30 },
+  { value: 10 },
+];
