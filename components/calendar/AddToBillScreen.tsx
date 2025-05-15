57256bfe626ab19017a46d18d98efde73dac4151
Prince Kumar <princesd991@gmail.com>
2025-05-15 22:17:09 +0530

    update add to bill screen

--- components/calendar/AddToBillScreen.tsx
+++ components/calendar/AddToBillScreen.tsx
@@ -1,3 +1,4 @@
+import { HeaderComponent } from "@/components/HeaderComponent";
 import React from "react";
 import {
   View,
@@ -5,31 +6,11 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
+  Image,
+  FlatList,
 } from "react-native";
 
 // Custom Icon Components
-const BackIcon = () => (
-  <View
-    style={{
-      width: 24,
-      height: 24,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <View
-      style={{
-        width: 10,
-        height: 10,
-        borderLeftWidth: 2,
-        borderBottomWidth: 2,
-        transform: [{ rotate: "45deg" }],
-        borderColor: "#000",
-      }}
-    />
-  </View>
-);
-
 const PlusIcon = () => (
   <View
     style={{
@@ -51,31 +32,21 @@ const PlusIcon = () => (
   </View>
 );
 
-const ChevronRightIcon = () => (
-  <View
-    style={{
-      width: 24,
-      height: 24,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <View
-      style={{
-        width: 8,
-        height: 8,
-        borderRightWidth: 2,
-        borderTopWidth: 2,
-        transform: [{ rotate: "45deg" }],
-        borderColor: "#fff",
-      }}
-    />
+const ProductService = ( item : any) => (
+  <View key={item.id} style={styles.itemCard}>
+    <View style={styles.itemDetails}>
+      <Text style={styles.itemTitle}>{item.title}</Text>
+      <Text style={styles.itemDescription}>{item.description}</Text>
+      <Text style={styles.itemPrice}>{item.price}</Text>
+    </View>
+    <TouchableOpacity style={styles.addButton}>
+      <PlusIcon />
+    </TouchableOpacity>
   </View>
 );
 
-// Step 1: Add to Bill Screen
 //{ onContinue, onBack }
-const AddToBillScreen = () => {
+const AddToBillScreen = ({ navigation }) => {
   const implantItems = [
     {
       id: 1,
@@ -117,39 +88,44 @@ const AddToBillScreen = () => {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.header}>
-        <TouchableOpacity
-          //  onPress={onBack}
-          style={styles.backButton}
-        >
-          <BackIcon />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Add to Bill</Text>
-      </View>
-
+      <HeaderComponent
+        title={"Add to Bill"}
+        rightButton={false}
+        onBackPress={() => {
+          navigation.goBack();
+        }}
+      />
       <View style={styles.progressContainer}>
-        <View style={styles.progressDot1} />
-        <View style={styles.progressDot2} />
-        <View style={styles.progressDot3} />
+        <Image source={require("../../assets/Union.png")} />
+        <Image source={require("../../assets/Union2.png")} />
+        <Image source={require("../../assets/Union2.png")} />
       </View>
 
       <TouchableOpacity style={styles.addItemsButton}>
         <Text style={styles.addItemsText}>Add to items</Text>
       </TouchableOpacity>
 
-      <Text style={styles.infoText}>
-        Add products, services or procedures. Prices can be changed in the next
-        step
-      </Text>
-
-      <View style={styles.searchContainer}>
-        <Text style={styles.searchText}>Search here</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>
+          Add products, services or procedures. Prices can be changed in the
+          next step
+        </Text>
       </View>
-
-      <Text style={styles.sectionTitle}>Product & Services</Text>
-
+      <View style={styles.searchMainContainer}>
+        <View style={styles.searchContainer}>
+          <Text style={styles.searchText}>Search here</Text>
+        </View>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.sectionTitle}>Product & Services</Text>
+      </View>
       <ScrollView style={styles.itemList}>
-        {implantItems.map(item => (
+        <FlatList
+          data={implantItems}
+          renderItem={({ item }) => ProductService(item)}
+          // keyExtractor={({item}:any) => item.id}
+        />
+        {/* {implantItems.map(item => (
           <View key={item.id} style={styles.itemCard}>
             <View style={styles.itemDetails}>
               <Text style={styles.itemTitle}>{item.title}</Text>
@@ -160,7 +136,7 @@ const AddToBillScreen = () => {
               <PlusIcon />
             </TouchableOpacity>
           </View>
-        ))}
+        ))} */}
       </ScrollView>
 
       <TouchableOpacity
@@ -169,7 +145,7 @@ const AddToBillScreen = () => {
       >
         <Text style={styles.continueButtonText}>Continue to Next Step</Text>
         <View style={styles.arrowContainer}>
-          <ChevronRightIcon />
+          <Image source={require("../../assets/forwardRight.png")} />
         </View>
       </TouchableOpacity>
     </View>
@@ -181,16 +157,32 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 62,
     padding: 6,
-    margin: 10,
-    backgroundColor: "#f5f5f5",
+    margin: 4,
+    backgroundColor: "#E8E9E9",
   },
   header: {
     flexDirection: "row",
     alignItems: "center",
     marginBottom: 16,
   },
+  backButtonContainer: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   backButton: {
     marginRight: 16,
+  },
+  headerTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    marginBottom: 10,
+    width: "60%",
   },
   headerTitle: {
     fontSize: 20,
@@ -201,24 +193,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     marginBottom: 20,
+    columnGap: 10,
+    marginTop: 10,
   },
   progressDot1: {
-    width: 24,
-    height: 24,
+    width: 14,
+    height: 14,
     borderRadius: 12,
     backgroundColor: "#8CD867",
     marginHorizontal: 8,
   },
   progressDot2: {
-    width: 24,
-    height: 24,
+    width: 14,
+    height: 14,
     borderRadius: 12,
     backgroundColor: "#D2EFC4",
     marginHorizontal: 8,
   },
   progressDot3: {
-    width: 24,
-    height: 24,
+    width: 14,
+    height: 14,
     borderRadius: 12,
     backgroundColor: "#D2EFC4",
     marginHorizontal: 8,
@@ -236,36 +230,52 @@ const styles = StyleSheet.create({
     color: "#000",
     fontWeight: "500",
   },
+  infoContainer: {
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  searchMainContainer: {
+    backgroundColor: "#fff",
+  },
   infoText: {
     textAlign: "center",
-    color: "#666",
+    paddingTop: 14,
+    fontWeight: "400",
     marginBottom: 16,
     paddingHorizontal: 32,
   },
   searchContainer: {
-    backgroundColor: "#e0e0e5",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#F1F5F9",
     borderRadius: 24,
     padding: 12,
+    margin: 8,
     marginBottom: 16,
+    height: 60,
   },
   searchText: {
     color: "#888",
     marginLeft: 8,
   },
   sectionTitle: {
+    backgroundColor: "#fff",
     fontSize: 16,
     fontWeight: "bold",
     marginBottom: 16,
+    padding: 10,
   },
   itemList: {
     flex: 1,
-    marginBottom: 20,
+    marginBottom: 10,
+    backgroundColor: "#fff",
   },
   itemCard: {
-    backgroundColor: "#fff",
-    borderRadius: 8,
+    backgroundColor: "#f8fafc",
+    borderRadius: 15,
     padding: 16,
-    marginBottom: 12,
+    margin: 10,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
@@ -275,40 +285,40 @@ const styles = StyleSheet.create({
   },
   itemTitle: {
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: "700",
   },
   itemDescription: {
-    fontSize: 12,
-    color: "#777",
+    fontSize: 14,
+    fontWeight: "400",
     marginVertical: 4,
   },
   itemPrice: {
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: "700",
   },
   addButton: {
-    padding: 8,
+    backgroundColor: "#ffff",
+    padding: 14,
+    borderRadius: 13,
   },
   continueButton: {
-    backgroundColor: "#8CD867",
+    backgroundColor: "#84CC16",
     borderRadius: 32,
     paddingVertical: 16,
     paddingHorizontal: 24,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginTop: 8,
-    marginBottom: 16,
   },
   continueButtonText: {
     color: "#fff",
-    fontWeight: "bold",
-    fontSize: 16,
+    fontWeight: "700",
+    fontSize: 18,
   },
   arrowContainer: {
-    backgroundColor: "rgba(255, 255, 255, 0.2)",
-    borderRadius: 16,
-    padding: 4,
+    width: 32,
+    height: 50,
+    marginEnd: 18,
   },
 });
 
